8b4b42c8b69f72134ccb2dac3cebea2e6c1d8f27
Max <max@debian-fx>
2021-05-06 17:10:40 +0200

    initial commit

---
+++ start_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# start busybox httpd and kill it when this script terminates
+trap "kill 0" EXIT
+busybox httpd -vv -f -p 8081 &
+
+while true; do
+	# minify the css
+	rm -f style.css style.min.css style.min.css.gz
+	./minify_css.sh *.css
+
+	# wait for file changes
+	inotifywait -e modify . 2> /dev/zero
+done
